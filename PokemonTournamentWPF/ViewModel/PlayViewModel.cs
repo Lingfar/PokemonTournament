56faf2b312d05125e0d70cf9a2a4ec74bdca5b43
Franck MOURRE <f13@laposte.net>
2017-02-10 11:38:00 +0100

    Premier push du mode de jeu [Non fonctionnel]

---
+++ PokemonTournamentWPF/ViewModel/PlayViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonTournamentEntities;
+
+namespace PokemonTournamentWPF.ViewModel
+{
+    public class PlayViewModel : ViewModelBase
+    {
+        public Partie Play
+        {
+            get
+            {
+                return Play;
+            }
+            set
+            {
+                // Rien à faire
+            }
+
+        }
+        private int self_hp { get; set; }
+        private int other_hp { get; set; }
+
+        PlayViewModel ()
+        {
+            self_hp = 3;
+            other_hp = 3;
+        }
+        void Add ( int attpos, int defpos )
+        {
+
+        }
+
+    }
+}
